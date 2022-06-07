c1c69630232ff2156fef5dba4a19d90bcc8c6ffa
danthorpe <danthorpe@users.noreply.github.com>
2022-06-07 23:13:29 +0100

    Using new Xcode 14 template

--- StarWars/StarWars/StarWarsApp.swift
+++ StarWars/StarWars/StarWarsApp.swift
@@ -4,7 +4,7 @@ import SwiftUI
 struct StarWarsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView(viewModel: .init())
+            ContentView()
         }
     }
 }
