baa3cd02c04aceecabe7382a53eb476fa24fb5b9
Daniel Thorpe <309420+danthorpe@users.noreply.github.com>
2023-09-22 08:05:15 +0100

    chore: Bump dependencies (#25)
    Co-authored-by: danthorpe <danthorpe@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -97,7 +97,7 @@ package.dependencies = [
     .package(url: "https://github.com/apple/swift-algorithms", from: "1.0.0"),
     .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.2"),
     .package(url: "https://github.com/apple/swift-async-algorithms", from: "0.1.0"),
-    .package(url: "https://github.com/apple/swift-http-types", from: "0.1.0"),
+    .package(url: "https://github.com/apple/swift-http-types", from: "1.0.0"),
     .package(url: "https://github.com/danthorpe/danthorpe-utilities", branch: "main"),
     .package(url: "https://github.com/danthorpe/danthorpe-swiftlint-plugin", from: "0.1.0"),
     .package(url: "https://github.com/pointfreeco/swift-concurrency-extras", from: "1.0.0"),
