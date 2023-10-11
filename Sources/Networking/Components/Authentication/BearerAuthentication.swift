f2a3e4d08fa9638206d7b13ed647ca6acde63516
Daniel Thorpe <309420+danthorpe@users.noreply.github.com>
2023-10-11 23:41:40 +0100

    feat: Add Authentication (#27)
    Co-authored-by: danthorpe <danthorpe@users.noreply.github.com>

---
+++ Sources/Networking/Components/Authentication/BearerAuthentication.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+extension AuthenticationMethod {
+  public static let bearer = AuthenticationMethod(rawValue: "Bearer")
+}
+
+public struct BearerCredentials: Hashable, Sendable, Codable, HTTPRequestDataOption, AuthenticatingCredentials {
+  public static let method: AuthenticationMethod = .bearer
+  public static let defaultOption: Self? = nil
+
+  public let token: String
+
+  public init(token: String) {
+    self.token = token
+  }
+
+  public func apply(to request: HTTPRequestData) -> HTTPRequestData {
+    var copy = request
+    copy.headerFields[.authorization] = "Bearer \(token)"
+    return copy
+  }
+}
+
+extension HTTPRequestData {
+  public var bearerCredentials: BearerCredentials? {
+    get { self[option: BearerCredentials.self] }
+    set { self[option: BearerCredentials.self] = newValue }
+  }
+}
+
+public typealias BearerAuthentication<
+  Delegate: AuthenticationDelegate
+> = HeaderBasedAuthentication<Delegate> where Delegate.Credentials == BearerCredentials
---
+++ Tests/NetworkingTests/Components/Authentication/BasicCredentialsTests.swift
@@ -0,0 +1,23 @@
+import AssertionExtras
+import Dependencies
+import Foundation
+import HTTPTypes
+import TestSupport
+import XCTest
+
+@testable import Networking
+
+final class BasicCredentialsTests: XCTestCase {
+  func test__apply_credentials() {
+    let credentials = BasicCredentials(user: "blob", password: "super!$3cret")
+    let request = credentials.apply(to: HTTPRequestData(id: "1"))
+    XCTAssertEqual(request.headerFields[.authorization], "Basic YmxvYjpzdXBlciEkM2NyZXQ=")
+  }
+
+  func test__provide_credentials() {
+    var request = HTTPRequestData(id: "1")
+    request.basicCredentials = BasicCredentials(user: "blob", password: "super!$3cret")
+    XCTAssertEqual(request.basicCredentials?.user, "blob")
+    XCTAssertEqual(request.basicCredentials?.password, "super!$3cret")
+  }
+}
