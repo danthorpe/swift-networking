93ee749d223640723b592e39029ef8846c084745
Daniel Thorpe <309420+danthorpe@users.noreply.github.com>
2023-10-12 23:06:24 +0100

    chore: Add authentication tests (#28)
    Co-authored-by: danthorpe <danthorpe@users.noreply.github.com>

--- Sources/TestSupport/Mocked.swift
+++ Sources/TestSupport/Mocked.swift
@@ -1,48 +1,57 @@
 import Networking
 
 extension NetworkingComponent {
-    public func mocked(
-        _ request: HTTPRequestData,
-        stub: StubbedResponseStream
-    ) -> some NetworkingComponent {
-        modified(Mocked(request: request, with: stub))
-    }
+  public func mocked(
+    _ stub: StubbedResponseStream,
+    check: @escaping (HTTPRequestData) -> Bool
+  ) -> some NetworkingComponent {
+    modified(Mocked(mock: check, with: stub))
+  }
 
-    public func mocked(
-        _ block: @escaping @Sendable (NetworkingComponent, HTTPRequestData) -> ResponseStream<HTTPResponseData>
-    ) -> some NetworkingComponent {
-        modified(CustomMocked(block: block))
-    }
+  public func mocked(
+    _ request: HTTPRequestData,
+    stub: StubbedResponseStream
+  ) -> some NetworkingComponent {
+    mocked(stub) { $0 ~= request }
+  }
 }
 
 struct Mocked: NetworkingModifier {
-    let mock: HTTPRequestData
-    let stub: StubbedResponseStream
+  let mock: (HTTPRequestData) -> Bool
+  let stub: StubbedResponseStream
 
-    @NetworkEnvironment(\.instrument) var instrument
+  @NetworkEnvironment(\.instrument) var instrument
 
-    init(request: HTTPRequestData, with stubbedResponse: StubbedResponseStream) {
-        self.mock = request
-        self.stub = stubbedResponse
+  init(mock: @escaping (HTTPRequestData) -> Bool, with stubbedResponse: StubbedResponseStream) {
+    self.mock = mock
+    self.stub = stubbedResponse
+  }
+
+  func send(upstream: NetworkingComponent, request: HTTPRequestData) -> ResponseStream<HTTPResponseData> {
+    guard mock(request) else {
+      return upstream.send(request)
     }
-
-    func send(upstream: NetworkingComponent, request: HTTPRequestData) -> ResponseStream<HTTPResponseData> {
-        guard request ~= mock else {
-            return upstream.send(request)
-        }
-        return ResponseStream { continuation in
-            Task {
-                await instrument?.measureElapsedTime("Mocked")
-                await stub(request).redirect(into: continuation)
-            }
-        }
+    return ResponseStream { continuation in
+      Task {
+        await instrument?.measureElapsedTime("Mocked")
+        await stub(request).redirect(into: continuation)
+      }
     }
+  }
+}
+
+extension NetworkingComponent {
+  public func mocked(
+    _ block: @escaping @Sendable (NetworkingComponent, HTTPRequestData) -> ResponseStream<HTTPResponseData>
+  ) -> some NetworkingComponent {
+    modified(CustomMocked(block: block))
+  }
 }
 
 struct CustomMocked: NetworkingModifier {
-    let block: @Sendable (NetworkingComponent, HTTPRequestData) -> ResponseStream<HTTPResponseData>
+  let block: @Sendable (NetworkingComponent, HTTPRequestData) -> ResponseStream<HTTPResponseData>
 
-    func send(upstream: NetworkingComponent, request: HTTPRequestData) -> ResponseStream<HTTPResponseData> {
-        block(upstream, request)
-    }
+  func send(upstream: NetworkingComponent, request: HTTPRequestData) -> ResponseStream<HTTPResponseData> {
+    block(upstream, request)
+  }
 }
