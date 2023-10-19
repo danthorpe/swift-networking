033b997d83ec7bfe267f008a6335ed336ce78021
Daniel Thorpe <309420+danthorpe@users.noreply.github.com>
2023-10-19 08:15:38 +0100

    chore: Use privacy mask when logging Authorization header (#29)
    Co-authored-by: danthorpe <danthorpe@users.noreply.github.com>

--- Sources/Networking/Components/Server.swift
+++ Sources/Networking/Components/Server.swift
@@ -3,57 +3,68 @@ import os.log
 
 extension NetworkingComponent {
 
-    public func server(authority: String?) -> some NetworkingComponent {
-        server(mutate: \.authority) { _ in
-            authority
-        } log: { logger, request in
-            logger?.info("💁 authority -> '\(authority ?? "no value")' \(request.debugDescription)")
-        }
+  public func server(authority: String?) -> some NetworkingComponent {
+    server(mutate: \.authority) { _ in
+      authority
+    } log: { logger, request in
+      logger?.info("💁 authority -> '\(authority ?? "no value")' \(request.debugDescription)")
     }
+  }
 
-    public func server(headerField name: HTTPField.Name, value: String?) -> some NetworkingComponent {
-        server(mutate: \.headerFields) { headers in
-            var copy = headers
-            copy[name] = value
-            return copy
-        } log: { logger, request in
-            logger?.info("💁 header \(name) -> '\(value ?? "no value")' \(request.debugDescription)")
-        }
+  public func server(headerField name: HTTPField.Name, value: String?) -> some NetworkingComponent {
+    server(mutate: \.headerFields) { headers in
+      var copy = headers
+      copy[name] = value
+      return copy
+    } log: { logger, request in
+      // swiftlint:disable line_length
+      guard let logger else { return }
+      guard name.requiresPrivateLogging else {
+        logger.info("💁 header \(name) -> '\(value ?? "no value", privacy: .public)' \(request.debugDescription)")
+        return
+      }
+      if name.requireHashPrivateLogging {
+        logger.info("💁 header \(name) -> '\(value ?? "no value", privacy: .private(mask: .hash))' \(request.debugDescription)")
+      } else {
+        logger.info("💁 header \(name) -> '\(value ?? "no value", privacy: .private)' \(request.debugDescription)")
+      }
+      // swiftlint:enable line_length
     }
+  }
 
-    public func server(prefixPath: String, delimiter: String = "/") -> some NetworkingComponent {
-        server(mutate: \.path) { path in
-            guard let path else { return prefixPath }
-            return prefixPath + delimiter + path
-        } log: { logger, request in
-            logger?.info("💁 prefix path -> '\(prefixPath)' \(request.debugDescription)")
-        }
+  public func server(prefixPath: String, delimiter: String = "/") -> some NetworkingComponent {
+    server(mutate: \.path) { path in
+      guard let path else { return prefixPath }
+      return prefixPath + delimiter + path
+    } log: { logger, request in
+      logger?.info("💁 prefix path -> '\(prefixPath)' \(request.debugDescription)")
     }
+  }
 
-    public func server<Value>(
-        mutate keypath: WritableKeyPath<HTTPRequestData, Value>,
-        with transform: @escaping (Value) -> Value,
-        log: @escaping (Logger?, HTTPRequestData) -> Void
-    ) -> some NetworkingComponent {
-        server { request in
-            @NetworkEnvironment(\.logger) var logger
-            request[keyPath: keypath] = transform(request[keyPath: keypath])
-            log(logger, request)
-        }
+  public func server<Value>(
+    mutate keypath: WritableKeyPath<HTTPRequestData, Value>,
+    with transform: @escaping (Value) -> Value,
+    log: @escaping (Logger?, HTTPRequestData) -> Void
+  ) -> some NetworkingComponent {
+    server { request in
+      @NetworkEnvironment(\.logger) var logger
+      request[keyPath: keypath] = transform(request[keyPath: keypath])
+      log(logger, request)
     }
+  }
 
-    public func server(
-        _ mutateRequest: @escaping (inout HTTPRequestData) -> Void
-    ) -> some NetworkingComponent {
-        modified(MutateRequest(mutate: mutateRequest))
-    }
+  public func server(
+    _ mutateRequest: @escaping (inout HTTPRequestData) -> Void
+  ) -> some NetworkingComponent {
+    modified(MutateRequest(mutate: mutateRequest))
+  }
 }
 
 private struct MutateRequest: NetworkingModifier {
-    let mutate: (inout HTTPRequestData) -> Void
-    func send(upstream: NetworkingComponent, request: HTTPRequestData) -> ResponseStream<HTTPResponseData> {
-        var copy = request
-        mutate(&copy)
-        return upstream.send(copy)
-    }
+  let mutate: (inout HTTPRequestData) -> Void
+  func send(upstream: NetworkingComponent, request: HTTPRequestData) -> ResponseStream<HTTPResponseData> {
+    var copy = request
+    mutate(&copy)
+    return upstream.send(copy)
+  }
 }
