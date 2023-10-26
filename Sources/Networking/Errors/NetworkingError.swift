1c22118da665c31b9f2a2f98a9e8b91fc0de47c6
danthorpe <danthorpe@users.noreply.github.com>
2023-10-26 08:54:47 +0100

    chore: Add conveniences to Error

--- Sources/Networking/Errors/NetworkingError.swift
+++ Sources/Networking/Errors/NetworkingError.swift
@@ -1,6 +1,37 @@
 import Foundation
+import Helpers
 
 public protocol NetworkingError: Error {
   var request: HTTPRequestData { get }
   var response: HTTPResponseData? { get }
+}
+
+// MARK: - Conveniences
+
+extension NetworkingError {
+
+  public var bodyStringRepresentation: String {
+    guard let response else {
+      return "no response"
+    }
+    guard response.data.isNotEmpty else {
+      return "empty body"
+    }
+    return String(decoding: response.data, as: UTF8.self)
+  }
+
+  public var isTimeoutError: Bool {
+    if let status = response?.status, status == .requestTimeout {
+      return true
+    }
+    switch self {
+    case let stackError as StackError:
+      if case .timeout = stackError {
+        return true
+      }
+      return false
+    default:
+      return false
+    }
+  }
 }
