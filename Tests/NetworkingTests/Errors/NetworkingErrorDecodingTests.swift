967801565dfc5dcfe9b07111666fb3cde6456633
Daniel Thorpe <309420+danthorpe@users.noreply.github.com>
2023-11-14 14:36:05 +0000

    chore: Improve test coverage (#41)
    Co-authored-by: danthorpe <danthorpe@users.noreply.github.com>

---
+++ Tests/NetworkingTests/Errors/NetworkingErrorDecodingTests.swift
@@ -0,0 +1,32 @@
+import Foundation
+import TestSupport
+import XCTest
+import os.log
+
+@testable import Networking
+
+final class NetworkingErrorDecodingTests: NetworkingTestCase {
+  override func invokeTest() {
+    withTestDependencies {
+      super.invokeTest()
+    }
+  }
+
+  func test__basics() throws {
+    struct ErrorMessage: Decodable {
+      let message: String
+    }
+
+    let data = try XCTUnwrap("""
+{"message":"There is an error"}
+""".data(using: .utf8))
+    let error = StubbedNetworkError(
+      url: URL(static: "https://example.com"),
+      data: data,
+      status: .badGateway
+    )
+
+    let errorMessage = try XCTUnwrap(error.decodeResponseBodyIntoJSON(as: ErrorMessage.self))
+    XCTAssertEqual(errorMessage.message, "There is an error")
+  }
+}
